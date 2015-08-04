db417ee595b61602ee0d2e184c3e397047ff5040
Nicksxs <Nicksxs@hotmail.com>
2015-08-04 22:04:21 +0800

    api controller
    add mobile app api

---
+++ App/Lib/Action/ApiAction.class.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nicks
+ * Date: 2015/8/4
+ * Time: 22:01
+ */
+    class ApiAction extends Action {
+        Public function rest() {
+            switch ($this->_method){
+                case 'get':
+
+                    break;
+                case 'post':
+
+                    break;
+                case 'put':
+
+                    break;
+                case 'delete':
+
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
